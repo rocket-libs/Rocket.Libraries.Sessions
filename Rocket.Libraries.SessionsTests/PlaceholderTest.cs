6662a3c1433ae470293d752e5017c3c0f71546b3
Nyingi Maina <nyingimaina@gmail.com>
2019-05-28 13:31:45 +0300

    Added a placeholder test to get dotnet test to work

---
+++ Rocket.Libraries.SessionsTests/PlaceholderTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Rocket.Libraries.SessionsTests
+{
+    public class PlaceholderTest
+    {
+        [Fact]
+        public void Passes()
+        {
+            Assert.True(true);
+        }
+    }
+}
