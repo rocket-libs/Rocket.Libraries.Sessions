bf4ad72f6b7283c603092412421778d9386dc67c
nyingi <nyingimaina@gmail.com>
2021-02-09 18:23:18 +0300

    Added functionality to inject sessions

---
+++ Rocket.Libraries.Sessions/SessionInjection/ISessionInjector.cs
@@ -0,0 +1,12 @@
+﻿using Rocket.Libraries.Sessions.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rocket.Libraries.Sessions.SessionInjection
+{
+    public interface ISessionInjector
+    {
+        string GetSessionSerialized { get; }
+    }
+}
---
+++ Rocket.Libraries.Sessions/SessionInjection/SessionInjectionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Rocket.Libraries.Sessions.Constants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.Libraries.Sessions.SessionInjection
+{
+    public class SessionInjectionMiddleware
+    {
+        private readonly RequestDelegate requestDelegate;
+
+        public SessionInjectionMiddleware(
+            RequestDelegate requestDelegate)
+        {
+            this.requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(
+            HttpContext httpContext,
+            ISessionInjector sessionInjector
+            )
+        {
+            var sessionSerialized = sessionInjector.GetSessionSerialized;
+            if (string.IsNullOrEmpty(sessionSerialized))
+            {
+                throw new Exception("No session was received for injection");
+            }
+            httpContext.Request.Headers.Add(
+                HeaderNameConstants.SessionInformation,
+                sessionSerialized);
+            await requestDelegate(httpContext);
+        }
+    }
+}
